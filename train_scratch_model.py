e57ceb42430f6cec49c5a0caf6f4a8668f498698
UG-Course Hyukjin001 <ee4483_02@login-2.cluster02.eee.ntu.edu.sg>
2025-11-12 17:25:20 +0000

    description

--- train_scratch_model.py
+++ train_scratch_model.py
@@ -1,6 +1,7 @@
 import os
 import torch
 import wandb
+import random
 import argparse
 from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
@@ -12,78 +13,82 @@ from torchvision import datasets, transforms
 
 from models.resnet import ResNet50
 
-# ---------------------------
-# 1) Variable Declaration
-# ---------------------------
-# TRAIN_DIR = "/projects/448302/datasets/train"
-# VAL_DIR   = "/projects/448302/datasets/val"
-# LEARNING_RATE = 1e-4
-# EPOCH = 50
-# BATCH_SIZE = 32
-
-# ---------------------------
-# 2) Image Preprocess
-# ---------------------------
-train_transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomRotation(degrees=15),
-    transforms.ToTensor(),
-    transforms.Normalize(
-        mean=[0.485, 0.456, 0.406],
-        std=[0.229, 0.224, 0.225]
-    )
-])
-### check
-val_transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize(
-        mean=[0.485, 0.456, 0.406],
-        std=[0.229, 0.224, 0.225]
-    )
-])
-
 
 def parse_args():
+    '''
+    1) Parse the input arguments
+    '''
     parser = argparse.ArgumentParser()
-
     parser.add_argument("--data_dir", type=str,
-                        default="/projects/448302/datasets")
+                        default="/projects/448302/datasets",
+                        help="path to the dataset directory; must have /train, /val, /test as child directory")
     parser.add_argument("--lr", type=float,
-                        default=1e-4)
+                        default=1e-4,
+                        help="learning rate")
     parser.add_argument("--epochs", type=int,
-                        default=50)
+                        default=50,
+                        help="training epoch")
     parser.add_argument("--batch_size", type=int,
-                        default=32)
+                        default=32,
+                        help="batch size")
     parser.add_argument("--save_dir", type=str,
-                        default="./runs")
+                        default="./runs",
+                        help="directory where the visualization will be saved")
     parser.add_argument("--wandb", type=str,
-                        default="_"
-    )
-
+                        default="_",
+                        help="whether to log using wandb, a visualization tool")
     return parser.parse_args()
 
 
-# ---------------------------
-# 3) Dataset / DataLoader
-# ---------------------------
 def load_data(data_dir=None):
+    '''
+    2) Load Dataset / Construct DataLoader
+    -   input
+            data_dir: (str) path to the dataset directory
+    -   output
+            train_loader: (torch.DataLoader) DataLoader for the training dataset
+            val_loader: (torch.DataLoader) DataLoader for the validation dataset
+    '''
     if data_dir is None: raise ValueError
     train_dir = data_dir + "/train"
     val_dir = data_dir + "/val"
+
+    train_transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(degrees=15),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
+    ])
+
+    val_transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
+    ])
 
     train_dataset = datasets.ImageFolder(train_dir, transform=train_transform)
     val_dataset   = datasets.ImageFolder(val_dir, transform=val_transform)
 
     train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=4)
     val_loader   = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, num_workers=4)
-
-    print("Classes:", train_dataset.classes)
     return train_loader, val_loader
 
 
 def visualize(loss: list[float], val_acc: list[float], save_dir: str):
+    '''
+    3) Code for Visualization of the result using matplotlib
+    -   input
+            loss: (list[float]) list of the loss history across the training epoch
+            vall_acc: (list[float]) list of the validation accuracy across the training epoch
+            data_dir: (str) path to the saving directory (where to save the generated figure of visualization)
+    '''
     fig, ax1 = plt.subplots(figsize=(10, 6))
     epochs = list(range(1, 1+len(loss)))
 
@@ -94,39 +99,46 @@ def visualize(loss: list[float], val_acc: list[float], save_dir: str):
     ax1.tick_params(axis='y', labelcolor=color)
     ax1.grid(True, linestyle='--', alpha=0.4)
 
-    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+    ax2 = ax1.twinx() # instantiate a second axes that shares the same x-axis to plot val accuracy and training loss concurrently
 
     color = 'tab:blue'
     ax2.set_ylabel('Val Accuracy (%)', color=color)  # we already handled the x-label with ax1
     ax2.plot(epochs, val_acc, color=color, label='Validation Accuracy')
     ax2.tick_params(axis='y', labelcolor=color)
 
-    # Calculate EMA5 for val_acc
+    # Calculate EMA5 for validation accuracy, to reduce a stochasticity in the performance
     val_acc_series = pd.Series(val_acc)
     ema5_val_acc = val_acc_series.ewm(span=5, adjust=False).mean()
 
     # Plot EMA5
-    ema_color = 'skyblue' # A lighter shade of blue
+    ema_color = 'skyblue'
     ax2.plot(epochs, ema5_val_acc, color=ema_color, linestyle='--', label='EMA5 Val Accuracy')
-
-
-    fig.tight_layout()  # otherwise the right y-label is slightly clipped
+    fig.tight_layout()
     plt.title('Training Loss and Validation Accuracy over Epochs')
 
     # Add legends
     handles1, labels1 = ax1.get_legend_handles_labels()
     handles2, labels2 = ax2.get_legend_handles_labels()
-    ax1.legend(handles1 + handles2, labels1 + labels2, loc='center right') # Changed legend location
-
+    ax1.legend(handles1 + handles2, labels1 + labels2, loc='center right')
 
     fname = save_dir + "/training_loss_val_acc.png"
     plt.savefig(fname)
     return
 
-# ---------------------------
-# 4) Train Function
-# ---------------------------
+
 def train_one_epoch(model, loader, criterion, optimizer, device):
+    '''
+    4) Training the model one epoch using the provided inputs
+    -   input
+            model: (torch.NN.module) neural network model that is the target of training
+            loader: (torch.DataLoader) dataloader to provide the data and target
+            criterion: (??) the objective loss function that the optimizer will minimize
+            optimizer: (torch.optim) an optimizer that will be used to train the neural network model
+            device: (str) a device that will be used for training (either CPU or CUDA-GPU)
+    -   output
+            total_loss: (float) the total loss across the epoch
+    '''
+
     model.train()
     total_loss = 0
 
@@ -179,6 +191,7 @@ def set_seed(seed=42):
     torch.backends.cudnn.benchmark = False
 
     print(f"Random Seed Fixed: {seed}")
+
 
 # ---------------------------
 # 7) Main Pipeline
